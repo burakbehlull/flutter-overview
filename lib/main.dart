ec9ec56a885fbf84d9a2d814fd220fef98e8667c
31000s <burakyabgu@gmail.com>
2023-11-11 23:47:12 +0300

    feat: Counter Application has been completed

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,50 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('İlk Flutter Uygulaması'),
-          ),
-          body: Text('Merhaba Flutter! Ben Burak!')),
+      home: MyCounterApp(),
+    );
+  }
+}
+
+class MyCounterApp extends StatefulWidget {
+  @override
+  _CounterState createState() => _CounterState();
+}
+
+class _CounterState extends State<MyCounterApp> {
+  int counter = 0; 
+
+  void arttir() {
+    setState(() {
+      counter++;
+    });
+  }
+
+  void azalt() {
+    setState(() {
+      counter--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Counter Uygulaması'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('$counter', style: TextStyle(fontSize: 20)),
+            ElevatedButton(
+              onPressed: arttir,
+              child: Icon(Icons.add),
+            ),
+            ElevatedButton(onPressed: azalt, child: Icon(Icons.remove)),
+          ],
+        ),
+      ),
     );
   }
 }
